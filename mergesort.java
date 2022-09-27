e3d2190256c5725c1a55cc349891d00c43109de7
vkmanic007 <63774994+vkmanic007@users.noreply.github.com>
2022-09-27 10:06:04 +0530

    Update mergesort.java

--- mergesort.java
+++ mergesort.java
@@ -80,7 +80,7 @@ class MergeSort {
 	// Driver code
 	public static void main(String args[])
 	{
-		int arr[] = { 12, 10, 13, 5, 6, 7 };
+		int arr[] = { 12, 10, 13, 5, 6, 9 };
 
 		System.out.println("Given Array");
 		printArray(arr);
