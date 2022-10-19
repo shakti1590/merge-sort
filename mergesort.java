8bb6a0c8273d71608c87f297f0ac5ef2c5305ed9
shrutigupta2713 <116139590+shrutigupta2713@users.noreply.github.com>
2022-10-19 14:45:39 +0530

    Update mergesort.java

--- mergesort.java
+++ mergesort.java
@@ -79,3 +79,4 @@ class MergeSort {
 }
 // The Merge Sort algorithm is a sorting algorithm that is based on the Divide and Conquer paradigm. In this algorithm, the array is initially divided into two equal halves and then they are combined in a sorted manner.
 /* This code is contributed by shakti Singh */
+//complexity:O(nlogn)
